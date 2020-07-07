169f85dacb513e844cbbdd190db8b1d2388410f8
mochamadyudi <myudisobari12@gmail.com>
2020-07-07 22:37:06 +0700

    [UPDATE]->[LAYOUT]&&[FUNCTION]

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
       'http://vodonesia.herokuapp.com',
       'vodonesia.id'
   ];
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN && '*');
+  res.header("Access-Control-Allow-Origin", process.env.ORIGIN || '*');
   res.header("Access-Control-Allow-Methods", 'GET, POST, PUT, DELETE');
   res.header("Access-Control-Allow-Credentials", 'true');
   next();
