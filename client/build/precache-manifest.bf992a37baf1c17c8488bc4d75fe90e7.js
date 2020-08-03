5619bbd10c326d1c13448b7f8775a6350f6d0a10
mochamadyudi <myudisobari12@gmail.com>
2020-08-04 06:56:59 +0700

    test chunk

---
+++ client/build/precache-manifest.bf992a37baf1c17c8488bc4d75fe90e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380b68756d3b1414cae233f92adf6e5c",
+    "url": "/pariwisataKabupatenKarawang/index.html"
+  },
+  {
+    "revision": "b4c1d6be83997b35a50b",
+    "url": "/pariwisataKabupatenKarawang/static/css/main.9ce09bc1.chunk.css"
+  },
+  {
+    "revision": "987021960a244e6b4051",
+    "url": "/pariwisataKabupatenKarawang/static/js/2.7eecc7f4.chunk.js"
+  },
+  {
+    "revision": "b4c1d6be83997b35a50b",
+    "url": "/pariwisataKabupatenKarawang/static/js/main.9b9a1737.chunk.js"
+  },
+  {
+    "revision": "6b825a788c05eb79a006",
+    "url": "/pariwisataKabupatenKarawang/static/js/runtime-main.b1b50f37.js"
+  },
+  {
+    "revision": "f5e47c09b798ae7fce3daa3182de2135",
+    "url": "/pariwisataKabupatenKarawang/static/media/SpinnerInterwind.f5e47c09.gif"
+  }
+]);
--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+const bundles = getBundles(stats, modules);
+const chunks = bundles.filter(bundle => bundle.file.endsWith('.js'))
+chunks.map(chunk => `<script src="/${chunk.file}"></script>`).join('\n')
+
+
 ReactDOM.render(<App />, document.getElementById("root"));
